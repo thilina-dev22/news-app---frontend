496f4ca3a825d8e633e1bf149446456e1a670fb3
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-18 20:54:21 +0530

    impliment newscard component

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
-const newsCard = (prop) => {
+const NewsCard = (prop) => {
+  const navigate = useNavigate();
+
+  const handleReadMore = (item) => {
+    console.log("clicked");
+    navigate(`/news?id=${item._id}`, { state: { item } });
+  };
+
   return (
     <div>
       {prop.news.map((item) => {
         return (
           <div key={item._id}>
-            <h3>{item.title}</h3>
+            <h2>{item.title}</h2>
             <p>{item.description}</p>
+            <button
+              onClick={() => {
+                handleReadMore(item);
+              }}
+            >
+              Read more...
+            </button>
           </div>
         );
       })}
@@ -15,4 +30,4 @@ const newsCard = (prop) => {
   );
 };
 
-export default newsCard;
+export default NewsCard;
